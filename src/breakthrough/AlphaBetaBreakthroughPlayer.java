13551ab247ff745c8c42a8f5c85728344c5d2bc0
Daltin Loomis <loomisdf@gmail.com>
2016-10-28 13:23:38 -0400

    add bare bones alpha beta player class

---
+++ src/breakthrough/AlphaBetaBreakthroughPlayer.java
@@ -0,0 +1,10 @@
+package breakthrough;
+
+/**
+ * Created by loomisdf on 10/28/2016.
+ */
+public class AlphaBetaBreakthroughPlayer {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
