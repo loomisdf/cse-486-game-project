62e38609a5abfdc1e985adf1b9ea828fbab3d0d3
Daltin Loomis <loomisdf@gmail.com>
2016-11-02 18:51:51 -0400

    Implement alpha beta pruning and time mechanics

--- src/breakthrough/MiniMaxBreakthroughPlayer.java
+++ src/breakthrough/MiniMaxBreakthroughPlayer.java
@@ -1,13 +1,10 @@
 package breakthrough;
 
-import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import game.GameMove;
 import game.GamePlayer;
 import game.GameState;
 
 import java.util.ArrayList;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
 
 import game.*;
 
@@ -16,15 +13,21 @@ import game.*;
  */
 public class MiniMaxBreakthroughPlayer extends GamePlayer {
     public final int MAX_DEPTH = 10;
+    public final int PANIC_TIME = 1000;
+    public final int MID_GAME_MOVE_TIME = 4000;
+    public final int EARLY_GAME_MOVE_TIME = 1000;
+    public final int LATE_GAME_TIME = 8000;
+    public static double moveTimeLeft;
     // We don't know how to figure out the max score
     private static final int MAX_SCORE = Integer.MAX_VALUE;
     public int depthLimit;
 
-    public ScoredBreakthroughMove mvStack[];
+    public ArrayList<ScoredBreakthroughMove> mvStack;
 
     public MiniMaxBreakthroughPlayer(String nname, int d)
     {
         super(nname, "Breakthrough");
+        moveTimeLeft = 240 * 1000; // ms
         depthLimit = d;
     }
 
@@ -62,9 +65,9 @@ public class MiniMaxBreakthroughPlayer extends GamePlayer {
      * Initializes the stack of Moves.
      */
     public void init() {
-        mvStack = new ScoredBreakthroughMove[MAX_DEPTH];
-        for (int i = 0; i < MAX_DEPTH; i++) {
-            mvStack[i] = new ScoredBreakthroughMove(0, 0, 0, 0, 0);
+        mvStack = new ArrayList<>(10);
+        for(int i = 0; i < 10; i++) {
+            mvStack.add(new ScoredBreakthroughMove(0, 0, 0, 0, 0));
         }
     }
 
@@ -95,15 +98,22 @@ public class MiniMaxBreakthroughPlayer extends GamePlayer {
         return isTerminal;
     }
 
-    public static int eval(BreakthroughState brd, char who) {
+    public static int eval(BreakthroughState state, char who) {
         int score = 0;
         for(int r = 0; r < BreakthroughState.N; r++) {
            for(int c = 0; c < BreakthroughState.N; c++) {
-               if(brd.board[r][c] == who) {
+               if(state.board[r][c] == who) {
+                   score+=5;
+               }
+               if(state.board[r][c] == who && (r == 3 || r == 4)) {
+                   score++;
+               }
+               if(state.board[r][c] == who && (r == 3 || r == 4) && (c == 3 || c == 4)) {
                    score++;
                }
            }
         }
+
         return score;
     }
 
@@ -151,9 +161,14 @@ public class MiniMaxBreakthroughPlayer extends GamePlayer {
         return list;
     }
 
-    private void minimax(BreakthroughState brd, int currDepth) {
+    private void minimax(BreakthroughState brd, double alpha, double beta, int currDepth, double timeRemaining) {
+        if(mvStack.size() < currDepth + 2) {
+            mvStack.add(new ScoredBreakthroughMove(0, 0, 0, 0, 0));
+        }
+        double beforeTime = System.currentTimeMillis();
         boolean toMaximize = (brd.getWho() == GameState.Who.HOME);
-        boolean isTerminal = terminalValue(brd, mvStack[currDepth]);
+        boolean toMinimize = !toMaximize;
+        boolean isTerminal = terminalValue(brd, mvStack.get(currDepth));
 
         if (currDepth == depthLimit) {
             System.out.println();
@@ -161,20 +176,17 @@ public class MiniMaxBreakthroughPlayer extends GamePlayer {
         if(isTerminal) {
 
         }
-        else if(currDepth == depthLimit) {
-            // Don't understand what this is supposed to represent
-           mvStack[currDepth].set(0, 0, 0, 0, evalBoard(brd));
+        else if(timeRemaining <= 0) {
+           mvStack.get(currDepth).set(0, 0, 0, 0, evalBoard(brd));
         }
         else {
 
             double bestScore = (brd.getWho() == GameState.Who.HOME ? Double.NEGATIVE_INFINITY
                     : Double.POSITIVE_INFINITY);
-            // How are the scores for bestMove and nextMove evaluated?
-            ScoredBreakthroughMove bestMove = mvStack[currDepth];
-            ScoredBreakthroughMove nextMove = mvStack[currDepth + 1];
+            ScoredBreakthroughMove bestMove = mvStack.get(currDepth);
+            ScoredBreakthroughMove nextMove = mvStack.get(currDepth + 1);
 
             bestMove.set(0, 0, 0, 0, bestScore);
-            GameState.Who currTurn = brd.getWho();
 
             ArrayList<BreakthroughMove> mvList = getAvailableMoves(brd);
             shuffle(mvList);
@@ -183,12 +195,28 @@ public class MiniMaxBreakthroughPlayer extends GamePlayer {
                 ScoredBreakthroughMove tempMv = new ScoredBreakthroughMove(mv);
                 BreakthroughState board_copy = (BreakthroughState)brd.clone();
                 board_copy.makeMove(tempMv);
-                minimax(board_copy, currDepth + 1);
+
+                double afterTime = System.currentTimeMillis();
+                double timeTaken = afterTime - beforeTime;
+                minimax(board_copy, alpha, beta, currDepth + 1, timeRemaining - timeTaken);
 
                 if (toMaximize && nextMove.score > bestMove.score) {
                     bestMove.set(tempMv.startRow, tempMv.startCol, tempMv.endingRow, tempMv.endingCol, nextMove.score);
                 } else if (!toMaximize && nextMove.score < bestMove.score) {
                     bestMove.set(tempMv.startRow, tempMv.startCol, tempMv.endingRow, tempMv.endingCol, nextMove.score);
+                }
+
+                // Update alpha and beta. Perform pruning, if possible.
+                if (toMinimize) {
+                    beta = Math.min(bestMove.score, beta);
+                    if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+                        return;
+                    }
+                } else {
+                    alpha = Math.max(bestMove.score, alpha);
+                    if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+                        return;
+                    }
                 }
             }
         }
@@ -196,20 +224,55 @@ public class MiniMaxBreakthroughPlayer extends GamePlayer {
 
     @Override
     public GameMove getMove(GameState state, String lastMv) {
-        minimax((BreakthroughState) state, 0);
-        try {
-            if(!state.moveOK(mvStack[0]))
-                throw new Exception("Move invalid");
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(state.numMoves == 0) {
+            moveTimeLeft = 240 * 1000;
         }
-        return mvStack[0];
+        double beforeTime = System.currentTimeMillis();
+        double timeLimit = calculateTimeLimit(state);
+
+        minimax((BreakthroughState) state, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0, timeLimit);
+
+        double timeTaken = System.currentTimeMillis() - beforeTime;
+//        try {
+//            if(!state.moveOK(mvStack.get(0)))
+//                throw new Exception("Move invalid");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        moveTimeLeft -= timeTaken;
+
+        System.out.println("Move time left: " + moveTimeLeft / 1000 + "secs");
+
+        return mvStack.get(0);
+    }
+
+    private double calculateTimeLimit(GameState state) {
+        double time = 1;
+        // PANIC
+        if(moveTimeLeft <= LATE_GAME_TIME + 1000) {
+            System.out.println("ENTERING PANIC MODE");
+           time = 1; // use 1 depth level
+        }
+        // check for beginning of game
+        else if(state.numMoves <= 4) {
+            time = EARLY_GAME_MOVE_TIME;
+        }
+        else if(state.numMoves > 4) {
+            if(moveTimeLeft <= 100 * 1000) {
+                time = LATE_GAME_TIME;
+            }
+            else {
+                time = MID_GAME_MOVE_TIME;
+            }
+        }
+        return time;
     }
 
     public static void main(String[] args) {
         GamePlayer p = new MiniMaxBreakthroughPlayer("Mini Max Player", 1);
         p.compete(args);
         System.out.println("Hi");
+//        p.solvePuzzles(new String [] {"BTPuzzle1", "BTPuzzle2"});
     }
 
 }
